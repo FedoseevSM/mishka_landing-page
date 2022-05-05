0c7d1f9bc3ad3533d7a32ca361f9700277574d43
Sorin Marcu <marcu.sorin.2003@gmail.com>
2022-05-05 15:23:27 +0300

    Добавил галп

---
+++ gulp/tasks/sass.js
@@ -0,0 +1,51 @@
+import dartSass from "sass";
+import gulpSass from "gulp-sass";
+import rename from "gulp-rename";
+
+import cleanCss from "gulp-clean-css";
+import webpcss from "gulp-webpcss";
+import autoprefixer from "gulp-autoprefixer";
+import groupCssMediaQueries from "gulp-group-css-media-queries";
+
+const scss = gulpSass(dartSass);
+
+export const sass = () => {
+  return app.gulp
+    .src(app.path.src.sass, { sourcemaps: true })
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "SASS",
+          message: "Error <%= error.message %>",
+        })
+      )
+    )
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(groupCssMediaQueries())
+    .pipe(
+      webpcss({
+        webpClass: ".webp",
+      })
+    )
+    .pipe(
+      autoprefixer({
+        grid: true,
+        overrideBrowserslist: ["last 3 versions"],
+        cascade: true,
+      })
+    )
+    .pipe(app.plugins.replace(/@img\//g, "../img/"))
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(cleanCss())
+    .pipe(
+      rename({
+        extname: ".min.css",
+      })
+    )
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream());
+};
