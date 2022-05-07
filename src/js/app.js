0c8c484ab44bbaa81e2a8c39b5551a345e041bbd
Sorin Marcu <marcu.sorin.2003@gmail.com>
2022-05-07 20:44:53 +0300

    test

--- src/js/app.js
+++ src/js/app.js
@@ -13,24 +13,3 @@ function isWebp() {
   });
 }
 isWebp();
-
-// header burger
-const header = document.querySelector(".header");
-const logoWrapper = document.querySelector(".header__logo_wrapper");
-const burgerBtn = document.querySelector(".header__burger");
-const mainWrapper = document.querySelector("main");
-let headerHeight = header.offsetHeight;
-mainWrapper.style.paddingTop = logoWrapper.offsetHeight + "px"; // Из-за того что шапка фиксированя, приходится дать пажинг для блоков через js
-function activeHeader(isActive) {
-  // Функия которая определяет если шапка активна и делает манипуляций в ней
-  if (isActive) {
-    header.style.maxHeight = headerHeight + "px";
-  } else {
-    header.style.maxHeight = logoWrapper.offsetHeight + "px";
-  }
-}
-activeHeader(header.classList.contains("_active"));
-burgerBtn.addEventListener("click", function () {
-  header.classList.toggle("_active");
-  activeHeader(header.classList.contains("_active"));
-});
