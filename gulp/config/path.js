ab6c6a792c75af1cd0b1a2360590ccee967c7306
samgxlden <samgxlden@gmail.com>
2022-07-08 18:45:16 +0300

    Убран_конфликт_в_js_файле

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,38 +1,37 @@
-// Getting the name of the project 
- 
-import * as nodePath from 'path';
-const rootFolder = nodePath.basename(nodePath.resolve());
+// Getting the name of the project
 
+import * as nodePath from "path"
+const rootFolder = nodePath.basename(nodePath.resolve())
 
-const buildFolder = `./${rootFolder}`;
-const srcFolder = "./src";
+const buildFolder = `./${rootFolder}`
+const srcFolder = "./src"
 
 export const path = {
-  build:{
+  build: {
     js: `${buildFolder}/js/`,
     css: `${buildFolder}/css/`,
     html: `${buildFolder}/`,
     files: `${buildFolder}/files/`,
     fonts: `${buildFolder}/fonts/`,
-    images: `${buildFolder}/img/`
+    images: `${buildFolder}/img/`,
   },
   src: {
-    js: `${srcFolder}/js/app.js`,
+    js: `${srcFolder}/js/*.js`,
     sass: `${srcFolder}/sass/style.sass`,
     html: `${srcFolder}/*.html`,
     files: `${srcFolder}/files/**/*.*`,
     images: `${srcFolder}/img/**/*.{png,jpg,jpeg,gif,webp}`,
-    svg: `${srcFolder}/img/**/*.svg`
+    svg: `${srcFolder}/img/**/*.svg`,
   },
-  watch:{
+  watch: {
     js: `${srcFolder}/**/*.js`,
     sass: `${srcFolder}/**/*.sass`,
     html: `${srcFolder}/**/*.html`,
     files: `${srcFolder}/files/**/*.*`,
-    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`
+    images: `${srcFolder}/img/**/*.{jpg,jpeg,png,svg,gif,ico,webp}`,
   },
   clean: buildFolder,
   buildFolder: buildFolder,
   srcFolder: srcFolder,
   rootFolder: rootFolder,
-};
+}
---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+/*--------Слайдер----------*/
+new Swiper(".reviewsSlider", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+})
+/*------------------*/
